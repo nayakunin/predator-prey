2dbbdcd757f34fa8dad7a59551a070a5505d499c
Nikita Yakunin <nayakunin@avito.ru>
2020-06-13 20:52:45 +0300

    Updates

---
+++ precache-manifest.7a144264ed9d407d9b6c1029088a8e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96669ca38896eb76bee689dc8ee9e2e",
+    "url": "/predator-prey/index.html"
+  },
+  {
+    "revision": "82d1ba1c81cfeca57e56",
+    "url": "/predator-prey/static/css/main.52703923.chunk.css"
+  },
+  {
+    "revision": "79322301201cc1fbf0e4",
+    "url": "/predator-prey/static/js/2.17029548.chunk.js"
+  },
+  {
+    "revision": "57d433a3cf738d4f2587da1d9252fc66",
+    "url": "/predator-prey/static/js/2.17029548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d1ba1c81cfeca57e56",
+    "url": "/predator-prey/static/js/main.d1fb99f4.chunk.js"
+  },
+  {
+    "revision": "01adf58d8a03858c39c5",
+    "url": "/predator-prey/static/js/runtime-main.348ef123.js"
+  }
+]);
